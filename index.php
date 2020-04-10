2c1738ac03861f4241d220b3553427b6fe4998b6
intiahmad92 <63441969+intiahmad92@users.noreply.github.com>
2020-04-10 11:35:23 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Coming Soon</title>
+</head>
+<body>
+<div style="text-align: center;margin-top:20%">
+	<h1 style="font-size:48px">Coming Soon.</h1>
+</div>
+</body>
+</html>
